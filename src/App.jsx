3b74fcb4650cb4906229c50e81a2d6976e965100
Riku <100498129+OOOgundam@users.noreply.github.com>
2022-10-23 07:06:03 +0000

    ddd

--- src/App.jsx
+++ src/App.jsx
@@ -9,155 +9,56 @@ function HomePage() {
         alignItems: 'center'
       }}
     >
-      <h1 style={{
-      fontFamily: 'fantasy', 
-      color: '#4b9be1' }}>
-        {"Riku's website"}
+      <h1
+        style={{ 
+        marginTop: '1rem',
+        fontFamily: 'monospace',
+        color: '#4b9be1' 
+      }}
+    >
+        Riku's website
       </h1>
       <h2
         style={{
+          marginBottom: '10rem',
           fontFamily: 'cursive',
-          color: 'rgb(243, 103, 123)',
-          marginBottom: '10rem'
+          color: 'rgb(243, 103, 123)'
         }}
       >
-        <h3 style={{
-        fontFamily:'fantasy',
-        color:'#3cd6bf'  }}>My website what I coded</h3>
-        <h4 style={{  
-         color:'#0d4fbf',
-         fontfamily:'cursive'  }>Hello</h4>
-        click the buttons below
+        Riku is very smart
       </h2>
       <button
         style={{
-          fontFamily: 'monospace',
           padding: '1rem',
           fontSize: '2rem',
           background: 'teal',
-          color: 'white'
+          color: 'red',
+          fontFamily: 'cursive'
         }}
         onClick={() => alert('Hello there')}
       >
-        Welcome
+        Hello!
       </button>
       <button
         style={{
-          fontFamily: 'monospace',
           padding: '1rem',
           fontSize: '2rem',
           background: 'purple',
-          color: 'white',
-          marginTop: '2rem'
+          color: 'blue',
+          marginTop: '2rem',
+          fontFamily: 'fantasy'
         }}
         onClick={() => {
           let name = prompt("What's your name?");
           if (name) {
             alert('Nice to meet you, ' + name + '!');
           } else {
-            alert('Nice to meet you, stranger');
+            alert('Nice to meet you, Riku!');
           }
         }}
       >
-        {"What's your name?"}
-      </button>
-      <button
-        style={{
-          fontFamily: 'monospace',
-          padding: '1rem',
-          fontSize: '2rem',
-          background: 'limegreen',
-          color: 'white'
-        }}
-        onClick={() => alert('Hello there')}
-      >
-        Welcome
-      </button>
-      <button
-        style={{
-          fontFamily: 'monospace',
-          padding: '1rem',
-          fontSize: '2rem',
-          background: 'black',
-          color: 'white',
-          marginTop: '2rem'
-        }}
-        onClick={() => {
-          let name = prompt("What's your name?");
-          if (name) {
-            alert('Nice to meet you, ' + name + '!');
-          } else {
-            alert('Nice to meet you, stranger');
-          }
-        }}
-      >
-        {"What's your name?"}
-      </button>
-      <button
-        style={{
-          fontFamily: 'monospace',
-          padding: '1rem',
-          fontSize: '2rem',
-          background: 'blue',
-          color: 'white'
-        }}
-        onClick={() => alert('Hello there')}
-      >
-        Welcome
-      </button>
-      <button
-        style={{
-          fontFamily: 'monospace',
-          padding: '1rem',
-          fontSize: '2rem',
-          background: 'orange',
-          color: 'white',
-          marginTop: '2rem'
-        }}
-        onClick={() => {
-          let name = prompt("What's your name?");
-          if (name) {
-            alert('Nice to meet you, ' + name + '!');
-          } else {
-            alert('Nice to meet you, stranger');
-          }
-        }}
-      >
-        {"What's your name?"}
-      </button>      <button
-        style={{
-          fontFamily: 'monospace',
-          padding: '1rem',
-          fontSize: '2rem',
-          background: 'silver',
-          color: 'white'
-        }}
-        onClick={() => alert('Hello there')}
-      >
-        Welcome
-      </button>
-      <button
-        style={{
-          fontFamily: 'monospace',
-          padding: '1rem',
-          fontSize: '2rem',
-          background: 'gold',
-          color: 'white',
-          marginTop: '2rem'
-        }}
-        onClick={() => {
-          let name = prompt("What's your name?");
-          if (name) {
-            alert('Nice to meet you, ' + name + '!');
-          } else {
-            alert('Nice to meet you, stranger');
-          }
-        }}
-      >
-        {"What's your name?"}
+        {"Hi! what's your name? My name is Riku."}
       </button>
     </div>
   );
 }
-
-export default HomePage;
